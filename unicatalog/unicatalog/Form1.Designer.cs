1e1c3f3a4fcd316a9090a20cbc2aef45cf93dc03
andrei_cosmin02 <andrei.achim.6669@gmail.com>
2023-06-16 13:18:25 +0300

    Sign in interface

--- unicatalog/unicatalog/Form1.Designer.cs
+++ unicatalog/unicatalog/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace unicatalog
 {
-    partial class Form1
+    partial class form_autentificare
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_signin = new System.Windows.Forms.Button();
+            this.label_invalid = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(71, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sign in to your account";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(63, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(63, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(63, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 29);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(63, 187);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(298, 29);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // btn_signin
+            // 
+            this.btn_signin.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_signin.FlatAppearance.BorderSize = 0;
+            this.btn_signin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_signin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_signin.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_signin.Location = new System.Drawing.Point(63, 270);
+            this.btn_signin.Name = "btn_signin";
+            this.btn_signin.Size = new System.Drawing.Size(298, 32);
+            this.btn_signin.TabIndex = 5;
+            this.btn_signin.Text = "Sign in";
+            this.btn_signin.UseVisualStyleBackColor = false;
+            this.btn_signin.Click += new System.EventHandler(this.btn_signin_Click);
+            // 
+            // label_invalid
+            // 
+            this.label_invalid.AutoSize = true;
+            this.label_invalid.ForeColor = System.Drawing.Color.Red;
+            this.label_invalid.Location = new System.Drawing.Point(126, 237);
+            this.label_invalid.Name = "label_invalid";
+            this.label_invalid.Size = new System.Drawing.Size(169, 15);
+            this.label_invalid.TabIndex = 6;
+            this.label_invalid.Text = "*Invalid username or password";
+            // 
+            // form_autentificare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(439, 354);
+            this.Controls.Add(this.label_invalid);
+            this.Controls.Add(this.btn_signin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "form_autentificare";
+            this.Opacity = 0.98D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Autentificare";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button btn_signin;
+        private Label label_invalid;
     }
 }
--- unicatalog/unicatalog/Form1.cs
+++ unicatalog/unicatalog/Form1.cs
@@ -1,10 +1,31 @@
 namespace unicatalog
 {
-    public partial class Form1 : Form
+    public partial class form_autentificare : Form
     {
-        public Form1()
+        public form_autentificare()
         {
             InitializeComponent();
+        }
+
+
+        private void btn_signin_Click(object sender, EventArgs e)
+        {
+            // Se deschide forms-ul cu content
+            var form2 = new Form2();
+            form2.Show();
+            this.Visible = false;
+        }
+
+        /// <summary>
+        /// Cand este apasat enter 
+        /// butonul de sign in este apasat automat
+        /// </summary>
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_signin_Click(sender, e);
+            }
         }
     }
 }
